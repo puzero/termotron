3d2903088c7a619d0a1774701ed51570d8810599
max.puzero <max.puzero@gmail.com>
2025-10-07 03:16:14 +0300

    Развернуты сервера, создана БД, создана система авторизации

--- back/app/Models/User.php
+++ back/app/Models/User.php
@@ -11,7 +11,6 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
